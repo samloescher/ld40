5122de244c764ae30a7c22b1429940d56038271f
Sam Loescher <samuel.loescher@gmail.com>
2017-12-02 10:59:33 +0000

    Initial commit

---
+++ html/src/src/ld40/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package src.ld40.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import src.ld40.MarsLander;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MarsLander();
+        }
+}
